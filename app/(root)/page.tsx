bc186ff34edc97e1af95845ad4505fe815e5c29c
Omegea Hunter <omegeadhunter@gmail.com>
2024-11-13 11:19:48 -0500

    "Added TotalBalanceBox component to Dashboard page, updated HeaderBox component to accept props, and fixed typo in TotalBalanceBoxProps interface"

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 const Dashboard= () => {
   const loggedIn = {firstName: 'Omegea', lastName: 'Hunter', username: 'UrbanJungleGoddess'};
   
@@ -12,6 +13,12 @@ const Dashboard= () => {
    title='Welcome'
    user= {loggedIn.username || 'Guest'}
    subtext='Your personal banking dashboard'
+   />
+   <TotalBalanceBox
+   accounts={[]}
+   totalBanks={22}
+   totalCurrentBalance={12345.95}
+
    />
   </header>
 </div>
--- components/HeaderBox.tsx
+++ components/HeaderBox.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
-const HeaderBox = () => {
+
+const HeaderBox = ({type = 'title', title, subtext, user}: HeaderBoxProps) => {
   return (
-    <div>HeaderBox</div>
+    <div className="header-box">
+       <h1 className="header-box-title">
+        {title}
+        {type === 'greeting' && 
+        (<span className="header-box-greeting text-bankGradient">
+            &nbsp;{user}
+            </span>
+        )}
+       </h1>
+         <p className="header-box-subtext">{subtext}</p>
+    </div>
   )
 }
 
