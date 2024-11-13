7be958984a8fba3ee63f5bc830adb8396d941284
Omegea Hunter <omegeadhunter@gmail.com>
2024-11-13 18:35:07 -0500

    Refactor RightSideBar component to enhance layout and display user profile information

--- components/RightSideBar.tsx
+++ components/RightSideBar.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const RightSideBar = ({user, transactions, banks}: RightSidebarProps) => {
   return (
-    <div>RightSideBar</div>
+    <aside className="right-sidebar">
+        <section className="flex flex-col-pb-8">
+            <div className="profile-banner" />
+<div className="profile">
+    <div className="profile-img">
+        <span className='text-5xl font-bold text-blue-500'>{user.firstName[0]}</span>
+    </div>
+</div>
+        </section>
+    </aside>
   )
 }
 
