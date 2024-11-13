7b9f4a10adf5b7e6b35dd8e23c457b5d061ac13b
Omegea Hunter <omegeadhunter@gmail.com>
2024-11-13 11:02:49 -0500

    "Added HeaderBox component to Dashboard page and updated layout"

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
-
-const Dashboard
- = () => {
+import HeaderBox from '@/components/HeaderBox'
+const Dashboard= () => {
+  const loggedIn = {firstName: 'Omegea', lastName: 'Hunter', username: 'UrbanJungleGoddess'};
+  
   return (
     <section className='home'>Dashboard
- <div>
-  
- </div>
+<div className="home-content">
+  <header className="home-header">
+   <HeaderBox
+   type='greeting'
+   title='Welcome'
+   user= {loggedIn.username || 'Guest'}
+   subtext='Your personal banking dashboard'
+   />
+  </header>
+</div>
     </section>
   )
 }
