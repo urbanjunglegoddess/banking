fd8f5d6706beb41e747d011dda1a9b5e625c9485
Omegea Hunter <omegeadhunter@gmail.com>
2024-11-13 19:24:59 -0500

    Add "Add Bank" section with icon to RightSideBar component

--- components/RightSideBar.tsx
+++ components/RightSideBar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
-
+import Image from 'next/image'
 const RightSideBar = ({user, transactions, banks}: RightSidebarProps) => {
   return (
     <aside className="right-sidebar">
@@ -23,8 +23,13 @@ const RightSideBar = ({user, transactions, banks}: RightSidebarProps) => {
         <section className="banks">
             <div className="flex w-full justify-between">
                 <h2 className="header-2">My Banks</h2>
-                <Link href="/" className='flex gap-2'>
+                <Link href="/" className="flex gap-2">
+                <Image 
+                src='/icons/plus.svg'
+                width={20} height={20}
+                alt='plus icon'/>
                 </Link>
+                <h2 className="text-14 font-semibold text-gray-600">Add Bank</h2>
             </div>
         </section>
     </aside>
