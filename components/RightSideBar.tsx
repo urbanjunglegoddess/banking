5b79f46025baca27c54f5c60fdfffb24d609da38
Omegea Hunter <omegeadhunter@gmail.com>
2024-11-13 19:33:50 -0500

    Add bank cards display to RightSideBar component

--- components/RightSideBar.tsx
+++ components/RightSideBar.tsx
@@ -20,17 +20,30 @@ const RightSideBar = ({user, transactions, banks}: RightSidebarProps) => {
                     </div>
                 </div>
         </section>
+
         <section className="banks">
             <div className="flex w-full justify-between">
                 <h2 className="header-2">My Banks</h2>
                 <Link href="/" className="flex gap-2">
                 <Image 
                 src='/icons/plus.svg'
-                width={20} height={20}
-                alt='plus icon'/>
-                </Link>
+                width={20} 
+                height={20}
+                alt='plus'
+                />
                 <h2 className="text-14 font-semibold text-gray-600">Add Bank</h2>
+                </Link>
             </div>
+            {banks?.length > 0  && (
+                <div className="relative flex flex-1 flex-col items-center justify-center gap-5">
+                    <div className="realtive z-10">
+                        BANK CARD 1
+                    </div>
+                    <div className="absolute right-0 top-8 z-0 w-[90%]">
+                        BANK CARD 2
+                    </div>
+                </div>
+            )}
         </section>
     </aside>
   )
