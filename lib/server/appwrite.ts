a703c2a731f4d5ee10c4172f26c3d236efa73e07
Omegea Hunter <omegeadhunter@gmail.com>
2024-11-14 17:20:51 -0500

    Enhance Appwrite client integration: add support for Databases and Users services in session client

--- lib/server/appwrite.ts
+++ lib/server/appwrite.ts
@@ -1,7 +1,8 @@
 // src/lib/server/appwrite.js
 "use server";
-import { Client, Account } from "node-appwrite";
+import { Client, Account, Databases } from "node-appwrite";
 import { cookies } from "next/headers";
+import { Users } from "node-appwrite";
 
 export async function createSessionClient() {
   const client = new Client()
@@ -32,5 +33,11 @@ export async function createAdminClient() {
     get account() {
       return new Account(client);
     },
-  };
-}
+    get database() {
+      return new Databases(client);
+    },
+    get user() {
+            return new Users(client);
+          },
+    };
+};
