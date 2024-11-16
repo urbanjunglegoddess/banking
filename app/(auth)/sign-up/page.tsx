3470eae928509187364d1e86a6e852dc58f868fd
Omegea Hunter <omegeadhunter@gmail.com>
2024-11-16 11:26:57 -0500

    Refactor sign-up process: update signUp action to handle user data more efficiently and simplify user account creation

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -6,6 +6,7 @@
 import AuthForm from '@/components/AuthForm'
 
 const SignUp = async () => {
+
   return (
     <section className="flex-center size-full max-sm:px-6">
       <AuthForm type="sign-up" />
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -26,43 +26,21 @@ export const signIn = async ({ email, password }: signInProps) => {
 }
 
 
-export const signUp = async ({ password, ...userData }: SignUpParams) => {
-  const { email, firstName, lastName } = userData;
+export const signUp = async (userData : SignUpParams) => {
+  const { email, password, firstName, lastName } = userData;
   
   let newUserAccount;
 
   try {
-    const { account, database } = await createAdminClient();
+    const { account } = await createAdminClient();
 
-    newUserAccount = await account.create(
+   const  newUserAccount = await account.create(
       ID.unique(), 
       email, 
       password, 
       `${firstName} ${lastName}`
     );
 
-    if(!newUserAccount) throw new Error('Error creating user')
-
-    const dwollaCustomerUrl = await createDwollaCustomer({
-      ...userData,
-      type: 'personal'
-    })
-
-    if(!dwollaCustomerUrl) throw new Error('Error creating Dwolla customer')
-
-    const dwollaCustomerId = extractCustomerIdFromUrl(dwollaCustomerUrl);
-
-    const newUser = await database.createDocument(
-      DATABASE_ID!,
-      USER_COLLECTION_ID!,
-      ID.unique(),
-      {
-        ...userData,
-        userId: newUserAccount.$id,
-        dwollaCustomerId,
-        dwollaCustomerUrl
-      }
-    )
 
     const session = await account.createEmailPasswordSession(email, password);
 
